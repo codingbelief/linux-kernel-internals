cec1ad59210cb4a1008cbbbdcff80e40120f5f4e
Rock Zhang <codingbelief@gmail.com>
2016-12-23 01:18:26 +0800

    Updates zh/js/custom.js
    Auto commit by GitBook Editor

--- zh/js/custom.js
+++ zh/js/custom.js
@@ -1,4 +1,4 @@
-/* custom js */
+/* baidu tongji js */
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
@@ -7,3 +7,16 @@ var _hmt = _hmt || [];
   s.parentNode.insertBefore(hm, s);
 })();
 
+/* baidu push */
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
